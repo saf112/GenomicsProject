dc3d57ec8156b3ea1073ae493f40762804a73e50
oacar <omeracar0@gmail.com>
2018-04-16 14:05:38 -0400

    removed clustering part

--- useFastGGM.R
+++ useFastGGM.R
@@ -42,33 +42,3 @@ outlist3 <- FastGGM(as.matrix(s3_sample[[1]]))
 #ggnet2(gg)
 
 
-####Clustering
-#Install necessary packages with install.packages("packageName") command first
-library(cluster)
-library(gplots)
-library(dendextend)
-library(lsa)
-epicells=readMat('/Users/omeracar/Box Sync/Genomics_project/sample_epicells.mat') #read file
-epicells=epicells[[1]]
-epiMatrix=as.matrix(epicells) #I am not sure if this is needed but using anyway, change data to matrix
-d1<-cosine(t(epiMatrix))
-d1<-1-d1 #calculate cosine similarity and (1-that) is dissimilarity
-d1<-as.dist(d1) #in order to use clustering function, convert d1 to dist object
-agn1 <- agnes(d1, method ="weighted" ) #agnes is the clustering function, change method for different linkages
-dend1_<-as.dendrogram(agn1) 
-
-out<-heatmap.2(epiMatrix,
-               #cellnote = mat_data,  # same data set for cell labels
-               #main = "agnes, manhattan", # heat map title
-               notecol="black",      # change font color of cell labels to black
-               density.info="none",  # turns off density plot inside color legend
-               trace="none",         # turns off trace lines inside the heat map
-               #scale = "row",
-               #margins =c(12,9),     # widens margins around plot
-               #col=hmcol,       # use on color palette defined earlier
-               #breaks=col_breaks,    # enable color transition at specified limits
-               dendrogram="row",     # only draw a row dendrogram
-               Rowv=dend1_, #
-               #RowSideColors=my_col,
-               Colv=FALSE,
-               cexRow = 0.3)
